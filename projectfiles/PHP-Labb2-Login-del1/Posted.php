470ab504de2a2e68c040e129bf5a734d182b6ad1
lr222gw <lr222gw@student.lnu.se>
2014-09-15 20:14:45 +0200

    Testfall 1.2 klart

---
+++ projectfiles/PHP-Labb2-Login-del1/Posted.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lowe
+ * Date: 2014-09-15
+ * Time: 19:44
+ */
+
--- projectfiles/PHP-Labb2-Login-del1/View/View.php
+++ projectfiles/PHP-Labb2-Login-del1/View/View.php
@@ -6,6 +6,7 @@
  * Time: 17:09
  */
 require_once("Model/Date.php");
+require_once("Posted.php");
 class view {
     private $model;
 
@@ -22,24 +23,44 @@ class view {
     }
 
     public function presentLoginForm(){
+        //hämtar ut datum
         $date = new Date();
         $date = $date->getDateTime(true);
+
+        //kollar om vi ska varna för tomt lösen/användarnamn
+        var_dump($_POST);
+        if(@trim($_POST["name"]) == "" && @isset($_POST["name"])){
+            $message = "<p>Användarnamn saknas</p>";
+        }else{
+            $message = "";
+        }
+        if(@trim($_POST["password"]) == "" && @isset($_POST["password"]) ){
+            $message2 = "<p>Lösenord saknas</p>";
+        }else{
+            $message2 ="";
+        }
+
+        //Htmln som ska åka ut på klienten
         $ToClient ="
                     <h3>Ej inloggad</h3>
-                    <form>
+                    $message
+                    $message2
+                    <form action='' method='post'>
                         <fieldset>
                             <legend>Login - Skriv in användarnamn och lösenord</legend>
                             <label for='name'>Namn</label>
-                            <input type='text' id='name' name='Namn'>
+                            <input type='text' id='name' name='name'>
                             <label for='pass'>Lösenord</label>
-                            <input type='text' id='pass' name='Lösenord'>
+                            <input type='text' id='pass' name='password'>
 
                         </fieldset>
+                        <input type='submit' value='Logga in' >
                     </form>
-                    <input type='submit'>
-                    <p>$date</p>
 
-        ";
+                    <p>$date</p>
+                    ";
+
+
 
         return $ToClient;
 
